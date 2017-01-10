9cc4575c6b4d919c7c12150184e7260c1e3a6599
DESKTOP-8T6G3SB\chiel <chiel1997@hotmail.com>
2017-01-10 12:00:33 +0100

    no message

--- home.php
+++ home.php
@@ -9,14 +9,14 @@ if (!isset($_SESSION['user'])) {
     exit;
 }
 // select loggedin users detail
-$res = mysql_query("SELECT * FROM users WHERE userEmail=" . $_SESSION['user']);
+$res = mysql_query("SELECT * FROM `users` WHERE userEmail = " . $_SESSION['user']);
 $userRow = mysql_fetch_array($res);
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Welcome - <?php echo $userRow['userEmail']; ?></title>
+        <title>Welcome - <?php echo $userRow['user']; ?></title>
     </head>
     <body>
         <p>Hi' <?php echo $userRow['userEmail']; ?></p>
