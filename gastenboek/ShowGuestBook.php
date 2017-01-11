1c6617f8352fda59fb38d72058271b2ab7014c4d
siem99 <siem.schippers@student.stenden.com>
2017-01-11 15:54:10 +0100

    je kan nu ook zien wat er in staat

---
+++ gastenboek/ShowGuestBook.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GuestBookPosts</title>
+    </head>
+    <body>
+        <?php
+            $DBConnect = mysqli_connect("localhost", "root", 
+            ""); 
+            if ($DBConnect === FALSE) 
+            { 
+                echo "<p>Unable to connect to the database 
+                server.</p>" 
+                . "<p>Error code " . mysqli_errno() 
+                . ": " . mysqli_error() 
+                . "</p>"; 
+            } else { 
+                $DBName = "guestbook"; 
+                if(!mysqli_select_db($DBConnect, $DBName)) 
+                { 
+                    echo "<p>There are no entries in the guest book!</p>"; 
+                }else { 
+                    $TableName = "visitors"; 
+                    $SQLstring = "SELECT * FROM $TableName"; 
+                    $QueryResult = mysqli_query($DBConnect, $SQLstring); 
+                    if (mysqli_num_rows($QueryResult) == 0) 
+                    { 
+                        echo "<p>There are no entries in 
+                        the guest 
+                        book!</p>"; 
+                    } else { 
+                        echo "<p>The following visitors have signed our "
+                        . "guest book:</p>"; 
+                        echo "<table width='100%' border='1'>"; 
+                        echo "<tr><th>First Name</th> "
+                        . "<th>Last "
+                        . "Name </th></tr>"; 
+                        while($Row = mysqli_fetch_assoc($QueryResult)) 
+                        { 
+                            echo "<tr><td>{$Row['first_name']}</td>"; 
+                            echo "<td>{$Row['last_name']}</td></tr>"; 
+                        } 
+                        mysqli_free_result($QueryResult); 
+                    }  
+                    mysqli_close($DBConnect); 
+                } 
+            }
+        ?>
+    </body>
+</html>
