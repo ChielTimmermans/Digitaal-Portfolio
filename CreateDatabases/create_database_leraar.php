bc0647da6e748e83b1b4b0ab66291f99c9e92aed
Handy Randy <radijkshoorn@gmail.com>
2017-01-15 14:37:01 +0100

    add klas en leraar tables

---
+++ CreateDatabases/create_database_leraar.php
@@ -0,0 +1,53 @@
+<?php
+$ipadress = "localhost";
+$ww = "";
+$DBConnect = mysqli_connect("$ipadress", "root", "$ww");
+if ($DBConnect === FALSE) {
+    echo "<p>Unable to connect to the database server.</p>"
+    . "<p>Error code " . mysqli_errno() . ": "
+    . mysqli_error() . "</p>";
+} else {
+    $DBName = "Portfolio";
+    if (!mysqli_select_db($DBConnect, $DBName)) {
+        $SQLstring = "CREATE DATABASE $DBName";
+        $QueryResult = mysqli_query($DBConnect, $SQLstring);
+        if ($QueryResult === FALSE) {
+            echo "<p>Unable to execute the query.</p>"
+            . "<p>Error code "
+            . mysqli_errno($DBConnect)
+            . ": " . mysqli_error($DBConnect) . "</p>";
+        } else {
+            echo "<p>You are the first visitor!</p>";
+        }
+    }
+    mysqli_select_db($DBConnect, $DBName);
+    $TableName = "leraren";
+    $SQLstring = "SHOW TABLES LIKE '$TableName'";
+    $QueryResult = mysqli_query($DBConnect, $SQLstring);
+    if (mysqli_num_rows($QueryResult) == 0) {
+        $SQLstring = "CREATE TABLE $TableName(
+                    userID          SMALLINT    NOT NULL        AUTO_INCREMENT  UNIQUE KEY,
+                    vak             Varchar(30),
+                    Voornaam        Varchar(30),
+                    Achternaam      Varchar(60),
+                    Email           Varchar(50) UNIQUE KEY,
+                    Mobielnummer    Int(15),
+                    Geboortedatum   Date,
+                    Adres           Varchar(30),
+                    Huisnummer      INT(5),
+                    Postcode        varchar(6),
+                    Woonplaats      varchar(30),
+                    Geslacht        ENUM('M','F'),
+                    Rol             int)";
+        $QueryResult = mysqli_query($DBConnect, $SQLstring);
+        if ($QueryResult === FALSE) {
+            echo "<p>Unable to create the table.</p>"
+            . "<p>Error code "
+            . mysqli_errno($DBConnect)
+            . ": " . mysqli_error($DBConnect) . "</p>";
+        }
+    }
+
+    mysqli_close($DBConnect);
+}
+
