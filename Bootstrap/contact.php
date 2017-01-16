61ae5c7ca6a8142b20637f34a2776e163217287c
PCHIEL\Chiel <chiel1997@hotmail.com>
2017-01-16 02:24:42 +0100

    no message

---
+++ Bootstrap/contact.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="dist/css/images/favicon.ico">
+
+        <title>Portfolio | Stenden Hogeschool</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <!-- Bootstrap core CSS -->
+        <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="dist/css/dashboard.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-fixed-top header-bg">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                     <a class="navbar-brand" href="https://stenden.com">
+                    <img src="images/stenden_logo1.png" alt="Stenden Logo">
+                </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="backend/student/home.html">Instellingen</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="#">English</a></li>
+                    </ul>
+        </div>
+      </div>
+    </nav>
+
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 col-md-2 sidebar">
+                    <ul class="nav nav-sidebar">
+						<li><a href="portfolio.html">Mijn Portfolio</a></li>
+                        <li><a href="projecten.html">Mijn Projecten</a></li>
+						<li><a href="cijfers.html">Mijn Cijferlijst</a></li>
+						<li><a href="gastenboek.html">Gastenboek</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                    <h1 class="page-header">Contact</h1>
+                </div>
+            </div></div>  
+
+
+        <div class="container">
+            <div class="col-sm-9 col-lg-12 col-sm-offset-3 col-md-12 col-md-offset-2 main">
+                <div class="panel panel-default">
+                    
+                    <div class="bs-callout bs-callout-danger">
+                        
+                            <div class="container">
+   
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="well">
+                <h3 style="line-height:20%;"><i class="fa fa-home fa-1x" style="line-height:6%;color:#339966"></i> Adres:</h3>               
+                <p style="margin-top:6%;line-height:35%">Van Schaikweg 94, Emmen</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-envelope fa-1x" style="line-height:6%;color:#339966"></i> E-Mail:</h3>
+                <p style="margin-top:6%;line-height:35%">emmen@stenden.com</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-user fa-1x" style="line-height:6%;color:#339966"></i> Telefoonnummer:</h3>
+                <p style="margin-top:6%;line-height:35%">0591 853 100</p>
+                <br />
+                <br />
+                <h3 style="line-height:20%;"><i class="fa fa-yelp fa-1x" style="line-height:6%;color:#339966"></i> Website:</h3>
+                <p style="margin-top:6%;line-height:35%"><a href="www.stenden.com/emmen">www.stenden.com/emmen</a></p>
+            </div>
+        </div>
+        <div class="col-md-6 hidden-sm">
+		<div class="iframe-container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2870.1084113422903!2d6.909820880133019!3d52.777943025057844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e61f3ec72443%3A0xbe9d297b3e4fbcc7!2sStenden+hogeschool+Emmen!5e0!3m2!1snl!2snl!4v1484216373026" width="565" height="430" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+            
+        </div>
+    </div>
+</div>
+       
+
+
+                    </div>
+                    
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
