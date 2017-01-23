1aab26a35323096eb78b7ea2b9884194ba2bdd31
DESKTOP-8T6G3SB\chiel <chiel1997@hotmail.com>
2017-01-23 10:24:29 +0100

    no message

---
+++ Bootstrap/leraarcijfertoevoegen.php
@@ -0,0 +1,69 @@
+<?php
+ob_start();
+session_start();
+//if (isset($_SESSION['user']) != "") {
+//    header("Location: ..\home.php");
+//}elseif ($row ['role'] != 'admin'){
+//    header("Location: ..\error.php");
+//}
+
+
+
+include_once '..\createdatabases/dbconnect.php';
+
+$error = false;
+
+if (isset($_POST['btn-signup']))
+{
+    
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Coding Cage - Login & Registration System</title>
+    </head>
+    <body>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+            <select name="klas">
+                <?php
+                include 'databaseArray.php';
+
+                $query = "SELECT Klas FROM klas";
+                $result = mysqli_query($conn, $query);
+
+                //Column 'Klas' omgezet naar een array.
+                $column = array();
+                while ($row = mysqli_fetch_array($result))
+                {
+                    $column[] = $row[Klas];
+                }
+                foreach($column as $res){
+                    echo $res;
+                    echo "<option value='$res'>$res</option>";
+                }
+                ?>
+            </select>
+            <select name="leerling">
+                <?php
+                $query ="SELECT Voornaam, Achternaam From gegevens where klas =$res";
+                                $result = mysqli_query($conn, $query);
+
+                //Column 'Klas' omgezet naar een array.
+                $column = array();
+                while ($row = mysqli_fetch_array($result))
+                {
+                    $column[] = $row[Klas];
+                }
+                foreach($column as $res){
+                    echo $res;
+                    echo "<option value='$res'>$res</option>";
+                }
+                ?>
+            </select
+            
+        </form>
+    </body>
+</html>
+<?php ob_end_flush(); ?>
