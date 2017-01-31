8c511434b9aeca3fac43ea04e317abc22706a3f6
PCHIEL\Chiel <chiel1997@hotmail.com>
2017-01-31 13:38:43 +0100

    no message

--- Bootstrap/redirected.php
+++ Bootstrap/redirected.php
@@ -15,19 +15,28 @@ if (!isset($_SESSION['user']))
         header("Location: backend\docent\home.php");
         echo"leraar";
         exit;
+    }elseif ($_SESSION['Rol'] == 3){
+        header("Location: backend\slber\home.php");
+        echo"leraar";
+        exit;
     }elseif ($_SESSION['Rol'] == 4){
         echo "admin";
         header("Location: ..\admin\adminkeuze.php");
         exit;
     }else{
-//        echo "rol session not set";
-//        echo ($_SESSION['Rol']);
-//        unset($_SESSION['user']);
-//        unset($_SESSION['Rol']);
-//        session_unset();
-//        session_destroy();
-//        //header("Location: index.php");
-//        exit;
+        echo "rol session not set";
+        echo ($_SESSION['Rol']);
+        unset($_SESSION['user']);
+        unset($_SESSION['Rol']);
+        session_unset();
+        session_destroy();
+        //header("Location: index.php");
+        exit;
     }
     
     }
+    
+    session_start();
+if (($_SESSION['Rol']) != "4"){
+    header("Location: ..\index.php");
+}
